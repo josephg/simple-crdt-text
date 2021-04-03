10c6d0fab8ab34959feac0b4e7fb2acd1bf8db74
Seph Gentle <me@josephg.com>
2021-04-03 17:30:46 +1100

    Added small tweaks to automerge-trace example

--- test/automerge-trace.ts
+++ test/automerge-trace.ts
@@ -21,7 +21,7 @@ let console = new Console({
 
 const checkBaseline = () => {
   let doc = ''
-  for (const [idx, remove, insert] of (edits as any as Edits)) {
+  for (const [idx, remove, insert] of edits) {
     doc = insert != null
       ? doc.slice(0, idx) + insert + doc.slice(idx + remove)
       : doc = doc.slice(0, idx) + doc.slice(idx + remove)
@@ -35,11 +35,12 @@ console.timeEnd('baseline')
 
 console.time('local crdt')
 {
+  console.log('\n*** WARNING This will take a really long time. It may never finish!\n')
   const doc = new Doc()
   console.log(`processing ${edits.length} edits...`)
   for (let i = 0; i < edits.length; i++) {
-    if (i % 1000 === 0) console.log(i)
-    const [idx, remove, insert] = (edits as any as Edits)[i]
+    if (i % 1000 === 0) console.log(`${i}: ${process.memoryUsage().rss / 1e6}mb in use`)
+    const [idx, remove, insert] = edits[i]
     assert(remove <= 1)
     const txn = insert != null
     ? doc.makeTxn('user', [doc.makeInsertOp(idx, insert!)])
